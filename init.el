b30c3a286d04a299be36703683828734a333ed03
Wanderson Ferreira <iagwanderson@gmail.com>
2020-11-27 20:18:52 -0300

    setup for magit after watching john's presentation

--- init.el
+++ init.el
@@ -125,8 +125,13 @@
   (set-default 'magit-revert-buffers 'silent)
   (set-default 'magit-no-confirm '(stage-all-changes
                                    unstage-all-changes))
+  (setq magit-save-repository-buffers t
+        magit-auto-revert-mode t
+        global-magit-file-mode t ;; enables C-x g, C-x M-g, C-c M-g
+        )
   :config
-  (global-set-key (kbd "C-c g s") 'magit-status))
+  (global-set-key (kbd "C-x g") 'magit-status)
+  )
 
 (use-package git-timemachine
   :ensure t)
